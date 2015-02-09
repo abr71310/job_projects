544100e2386be0563f65975541c7936cc32b2fce
Michael Shao <me@michaelshao.com>
2015-02-09 01:48:11 -0500

    last push... can't get 3-6 working as intended (same with 7-9)

--- thumbtack/README.md
+++ thumbtack/README.md
@@ -20,7 +20,8 @@ The test inputs after in6.txt are my own special edge cases:
 * in9.txt
 
 The outputs all correspond to the input files, with the format "out[#].txt".  
-The intention behind this was to ensure consistency when testing.
+The intention behind this was to ensure consistency when testing.  
+Side note: I am not well-versed in Python, so my code is not as well-optimized as I had wanted.  
 
 Also, to whoever is marking this: I didn't see the e-mail sent to me dated January 19th until this last Friday (February 6).  
 I hope it's still OK that I'm submitting this (extremely late, since it said only take a week!) now.
--- thumbtack/database-challenge.py
+++ thumbtack/database-challenge.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
-# Thumbtack challenge solution : Simple Database
-# Michael Shao
-# Modified: 02/08/2015
-# Version: 0.0.0.0
+# Thumbtack : Simple Database Challenge
+# Author: Michael Shao
+# Modified: 02/09/2015 (Time elapsed: 3 hours 25 minutes)
+# Version: 1.1.0.1
 
 import sys
 
 class Database(object):
 	# Constructor
-	def __init__(self):
-		super(Database, self).__init__()
+	def __init__(self, parent = None):
+		self.parent = parent
 		
+		# For committing things (test for "no transaction")
+		self.commits = []
+		# Key-val pairs (base-level storage)
 		self.values = {}
+		# In case we unset things
 		self.unset = []
 		
 	# sets variable name <key> to <value>
@@ -20,7 +24,7 @@ class Database(object):
 	def set(self, key, value):
 		# Assign a new value
 		self.values[key] = value
-		
+		self.commits.append(key)
 		# Remove key from the unset stack, assuming it was unset.
 		if key in self.unset:
 			self.unset.pop(key)
@@ -54,21 +58,72 @@ class Database(object):
 		# If it exists in our keys, return it.
 		if key in self.values.keys():
 			return self.values[key]
+		elif self.parent:
+			return self.parent.get_valfromkey(key);
 		# If we don't have that value, return None.
 		return None
+		
+	def commit(self):
+		# commit current transactions to super! 
+		for key in self.values:
+			self.parent.values[key] = self.values[key]
+		for key in self.unset:
+			self.parent.values.pop(key)
 
 if __name__ == "__main__":
-	data = Database()
+	main_db = Database()
+	cur_db = main_db
 	parse_line = sys.stdin.readline().strip()
 	while parse_line != 'END':
 		# Split arguments by single whitespace character (' ')
 		args = parse_line.split(' ')
 		# Debugging purposes ( comment out before submitting )
 		#print args
-		legalCmds = ['BEGIN', 'SET', 'GET', 'UNSET', 'NUMEQUALTO']
+		legalCmds = ['SET', 'GET', 'UNSET', 'NUMEQUALTO', 'BEGIN', 'ROLLBACK', 'COMMIT']
 		if args[0] in legalCmds:
-			f = getattr(Database, args[0].lower())
-			f(data,*args[1:])
+			if args[0] == 'ROLLBACK':
+				if cur_db.commits == []:
+					print "NO TRANSACTION"
+				else:
+					# Store old record in a temporary variable
+					temp_db = cur_db.parent
+					# Kill current DB - we're rolling back!
+					del cur_db
+					# Now set our current DB to the temp var
+					cur_db = temp_db
+			elif args[0] == 'BEGIN':
+				# Create new DB with current DB as the "parent" (if it exists)
+				new_db = Database(cur_db)
+				# Create a pointer just in case
+				cur_db = new_db
+			elif args[0] == 'COMMIT':
+				if cur_db.values == {}:
+					print "NO TRANSACTION"
+				else:
+					# Commit current (we're going to sanitize things)
+					cur_db.commit()
+					# create a temp
+					temp_db = cur_db
+					# set parent of current to current
+					cur_db = cur_db.parent
+					# delete the temp
+					del temp_db
+					cur_db.commits = []
+			#elif args[0] == 'SET':
+				#cur_db.set(args[1], args[2])
+			#elif args[0] == 'UNSET':
+				#cur_db.unset(args[1])
+			#elif args[0] == 'GET':
+				#cur_db.get(args[1])
+			#elif args[0] == 'NUMEQUALTO':
+				#cur_db.numequalto(args[1])
+			else: 
+				fn = getattr(Database, args[0].lower())
+				fn(cur_db,*args[1:])
+				#print "argument", args[0] , "params: ", args[1:]
+				#print "cur_db: ", cur_db.values
+				#print "main_db: ", main_db.values
+				#print "Database?? ", Database
 		else:
 			print "Invalid command / arguments: %s" % args
 			
